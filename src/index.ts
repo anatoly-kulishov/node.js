c798da013ef23e66218df69842b487a739aa55d1
Anatoly Kulishov <anatoly.kulishov@external.tele2.ru>
2024-07-09 16:15:31 +0300

    1.0.4

--- src/index.ts
+++ src/index.ts
@@ -15,90 +15,10 @@ const HTTP_STATUSES = {
 const jsonBodyMiddleware = express.json()
 app.use(jsonBodyMiddleware)
 
-let db = {
-    courses: [
-        {id: 1, title: 'Front-end'},
-        {id: 2, title: 'Back-end'},
-        {id: 3, title: 'DevOps'},
-        {id: 4, title: 'QA'}
-    ]
-}
-
-app.get('/courses', (req, res) => {
-    let foundCoursesQuery = db.courses;
-
-    if (req.query.title) {
-        const titleQuery = req.query.title as string
-        foundCoursesQuery = foundCoursesQuery
-            .filter(c => c.title.toLowerCase().includes(titleQuery.toLowerCase()))
-    }
-
-    if (!foundCoursesQuery.length) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return;
-    }
-
-    res.json(foundCoursesQuery)
+app.get('/', (req, res) => {
+    res.sendStatus(404)
 })
 
-app.get('/courses/:id', (req, res) => {
-    const foundCourse = db.courses.find(c => c.id === Number(req.params.id))
-
-    if (!foundCourse) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return;
-    }
-
-    res.json(foundCourse)
-})
-
-app.post('/courses', (req, res) => {
-    const titleBodyParam = req.body?.title.trim()
-
-    if (!titleBodyParam) {
-        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
-        return;
-    }
-
-    let createdCourse = {
-        id: Number(new Date()),
-        title: titleBodyParam
-    };
-    db.courses.push(createdCourse)
-    res.status(HTTP_STATUSES.CREATED_201).send(createdCourse)
-})
-
-app.delete('/courses/:id', (req, res) => {
-    const filteredCourses = db.courses.filter(c => c.id !== Number(req.params.id))
-
-    if (db.courses.length === filteredCourses.length) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-    }
-
-    db.courses = filteredCourses
-
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-})
-
-app.put('/courses/:id', (req, res) => {
-    const titleBodyParam = req.body?.title.trim()
-
-    if (!titleBodyParam) {
-        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
-        return;
-    }
-
-    const foundCourse = db.courses.find(c => c.id === Number(req.params.id))
-
-    if (!foundCourse) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return;
-    }
-
-    foundCourse.title = titleBodyParam
-
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
